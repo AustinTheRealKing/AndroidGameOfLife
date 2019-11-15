81f160cb6d873fc52a96c1b9904d846661fe3248
Austin King <austin.king@mymail.champlain.edu>
2019-11-14 21:11:16 -0500

    Cleaned up code base

--- MyApplication/app/src/main/java/com/example/myapplication/GameOfLifeActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/GameOfLifeActivity.java
@@ -1,8 +1,5 @@
 package com.example.myapplication;
 
-import android.content.Context;
-import android.content.Intent;
-
 import androidx.fragment.app.Fragment;
 
 public class GameOfLifeActivity extends SingleFragmentActivity {
